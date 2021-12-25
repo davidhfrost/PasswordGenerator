64cdf4de594e7e290f700169a2f7fd68f27bd1e8
davidhfrost <82253398+davidhfrost@users.noreply.github.com>
2021-12-24 19:50:33 -0500

    added more html and flask

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -7,4 +7,25 @@ app = Flask(__name__)
 def home():
     return render_template("primary.html")
 
-#def generate():
+
+@app.route("/generate", methods=["POST"])
+def generate():
+    uc = request.form.get("uppercase")
+    nu = request.form.get("numbers")
+
+    return render_template("generated.html")
+    """pref = request.args.get("uppercase")
+    print(pref)
+    if (request.method == 'POST'):
+        test = request.form['uppercase']
+        print("Hello world")
+        return "Hi"
+    # your code
+    # return a response
+    """
+
+
+# def generate():
+
+if __name__ == '__main__':
+    app.run()
