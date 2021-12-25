5daca549b9e7cc3fe7c2619f7993a13f012fe247
davidhfrost <82253398+davidhfrost@users.noreply.github.com>
2021-12-24 20:05:04 -0500

    fixed some bugs with lists and html

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, url_for, redirect
+from secrets import choice, randbelow
 
 app = Flask(__name__)
 
@@ -10,10 +11,18 @@ def home():
 
 @app.route("/generate", methods=["POST"])
 def generate():
+    charList = []
     uc = request.form.get("uppercase")
+    lc = request.form.get("lowercase")
     nu = request.form.get("numbers")
-
-    return render_template("generated.html")
+    if lc is not None:
+        charList.extend(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+    if nu is not None:
+        charList.extend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+    password = ""
+    #for i in range(10):
+        #password = password + charList[randbelow(36)]
+    return render_template("generated.html", passw=charList)
     """pref = request.args.get("uppercase")
     print(pref)
     if (request.method == 'POST'):
